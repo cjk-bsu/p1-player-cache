1cf4c4dc6542c789fca6c5955685f5da1e3b85bb
Carson Keller <carsonjkeller@gmail.com>
2023-01-27 15:15:45 -0700

    Added necessary documentation and javadocs

--- CacheTest.java
+++ CacheTest.java
@@ -3,14 +3,34 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
+/**
+ * This class acts as the main driver for the creation and implementation of the
+ * Cache class to act as a cache for serialized data being read in from provided
+ * filenames given a provided cache size.
+ *
+ * @author carsonkeller
+ */
 public class CacheTest {
     private static long startTime, endTime;
     private static Cache<Player> cache;
 
+    /**
+     * This method prints usage instructions to the console.
+     */
     private static void printUsage() {
         System.out.println("java CacheTest <cache-size> <serialized-data-filename>");
     }
 
+    /**
+     * This main method parses the required command line arguments, instantiates a
+     * new cache of the specified size, deserializes and reads in the contents of a
+     * specified file to be added to that cache. The necessary command line args
+     * consist of: 1. the integer value of the cache size or the number of objects
+     * it can hold, and 2. the name of the file to be read in.
+     *
+     * @param args - string values for both the integer value of the cache size
+     *               followed by the filename of the data file to be read in
+     */
     public static void main(String[] args) {
         if (args.length != 2) {
             printUsage();
